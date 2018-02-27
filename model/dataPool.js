da0c5cac6f73c9eb30ea2f589ac16e7ca31a8ce2
guzhongren <guzhongren@live.cn>
2018-02-27 18:07:33 +0800

    数据库查询重新调整

--- model/dataPool.js
+++ model/dataPool.js
@@ -4,22 +4,25 @@ class DataBasePool {
   constructor () {
     // this._dataBaseConfig = config.dataBase
     // this._instance = null
-    this.pool = pg.Pool(config.dataBase)
+    this.pool = {}
   }
 
-  static getDataBasePoolInstance () {
-    if (!DataBasePool.instance) {
-      DataBasePool.instance = new DataBasePool()
-      // pool.on('error', (err) => {
-      //   console.error('Unexpected error on edle client', err)
-      // })
-      // pool.connect().then((client) => {
-      //   return client
-      // }, (err) => {
-      //   return console.log('获取数据库客户端出错', err)
-      // })
-    }
-    return DataBasePool.instance.pool
+  static getInstance () {
+    return new Promise((resolve, reject) => {
+      if (!DataBasePool.instance) {
+        DataBasePool.instance = new DataBasePool()
+        try {
+          DataBasePool.pool = pg.Pool(config.dataBase)
+          resolve(DataBasePool.pool)
+        } catch (err) {
+          let res = {}
+          res.status = 500
+          res.message = `数据库连接出错${err}`
+          reject(res)
+        }
+      }
+      resolve(DataBasePool.pool)
+    })
   }
 }
 
---
+++ model/userModel.js
@@ -0,0 +1,48 @@
+'use strict'
+import DataBasePool from './dataPool'
+class User {
+  constructor() {
+    this.name = 'aa'
+  }
+  /**
+   * 获取所有用户信息
+   * @returns Promise
+   * @memberof User
+   */
+  getUsers () {
+    return new Promise((resolve, reject) => {
+      DataBasePool.getInstance().then((pool) => {
+        pool.connect().then((client) => {
+          console.log('test', client.err)
+          if (client.err) {
+            let e = {}
+            e.status = 500
+            e.message = `查询客户端出错${client.err}`
+            reject(e)
+          }
+
+          client.query(`SELECT * FROM public.irrg_bu_userinfo;`)
+            .then((sqlResult) => {
+              let res = {}
+              if (sqlResult.rowCount >= 0) {
+                res.status = 'success'
+                res.data = sqlResult.rows
+              }
+              client.release(true)
+              resolve(res)
+            })
+            .catch(err => {
+              let e = {}
+              e.status = 500
+              e.message = `查询语句执行出错${err}`
+              reject(e)
+            })
+        })
+      }, (err) => {
+        reject(err)
+      })
+    })
+  }
+}
+
+export default User
